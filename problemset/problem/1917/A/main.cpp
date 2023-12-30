b0e33dcec351f3a8c34d83f09f8e24faf8b6b97a
DryvoeVedro <66407002+DryvoeVedro@users.noreply.github.com>
2023-12-30 21:24:33 +1000

    New solution

---
+++ problemset/problem/1917/A/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+
+
+
+
+int main() {
+    std::cout << "sdasd"<<std::endl;
+}
