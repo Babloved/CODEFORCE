ac5a90f561ef82e83d5ab16af34907ac36448448
DryvoeVedro <66407002+DryvoeVedro@users.noreply.github.com>
2024-06-03 22:27:50 +1000

    nice

--- problemset/problem/1338/A/main.cpp
+++ problemset/problem/1338/A/main.cpp
@@ -4,37 +4,37 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-    int x=0,t=0,n=0,maxTime=-1;
-    double temp=0;
-    cin>>t;
+    int x = 0, t = 0, n = 0, maxTime = -1;
+    double temp = 0;
+    cin >> t;
     vector<int> result(t);
     for (int j = 0; j < t; ++j) {
-        cin>>n;
-        maxTime=-1;
+        cin >> n;
+        maxTime = -1;
         vector<long long> a(n);
         vector<long long> b(n);
         for (int i = 0; i < n; ++i) {
 
-            cin>>a[i];
+            cin >> a[i];
             b[i] = -1;
-            if (i != 0){
-                while (a[i-1]>a[i]){
+            if (i != 0) {
+                while (a[i - 1] > a[i]) {
 //                    temp = log(a[i-1]-a[i])/log(2);
-                    b[i] = log(a[i-1]-a[i])/log(2);
-                    b[i]>maxTime?maxTime=b[i]:0;
-                    a[i]=a[i]+pow(2,b[i]);
+                    b[i] = log(a[i - 1] - a[i]) / log(2);
+                    b[i] > maxTime ? maxTime = b[i] : 0;
+                    a[i] = a[i] + pow(2, b[i]);
                 }
-            }else{
-                a[i]=a[i];
+            } else {
+                a[i] = a[i];
             }
         }
-        result[j] = maxTime+1;
+        result[j] = maxTime + 1;
 
     }
-    for (auto el:result) {
-        cout<<el<<endl;
+    for (auto el: result) {
+        cout << el << endl;
     }
     return 0;
 }
--- problemset/problem/1353/D/main.cpp
+++ problemset/problem/1353/D/main.cpp
@@ -7,67 +7,59 @@
 #include <tuple>
 
 using namespace std;
-class CoordData{
-public:
-    int range,coordinate;
 
+class CoordData {
+public:
+    int range, coordinate;
 };
 
 class compCoordData {
 public:
-    bool operator()(const CoordData &lhs,const CoordData &rhs)
-    {
-        if (lhs.range!=rhs.range ) {
-            return lhs.range<rhs.range; //whatever
-        }else{
-            return lhs.coordinate>rhs.coordinate;
+    bool operator()(const CoordData &lhs, const CoordData &rhs) {
+        if (lhs.range != rhs.range) {
+            return lhs.range < rhs.range; //whatever
+        } else {
+            return lhs.coordinate > rhs.coordinate;
         }
     }
 };
 
+int main() {
 
-int main(){
-
-    int t,a,coordinate,range,midRange;
+    int t, a, coordinate, range, midRange;
     cin >> t;
-    priority_queue<CoordData,vector<CoordData>, compCoordData> dMidRange;
-
+    priority_queue<CoordData, vector<CoordData>, compCoordData> dMidRange;
     vector<vector<int>> vResult;
-
     for (int i = 0; i < t; ++i) {
-        cin>>a;
-        dMidRange.push({a,0});
-        vector<int>vec(a);
+        cin >> a;
+        dMidRange.push({a, 0});
+        vector<int> vec(a);
         for (int j = 0; !dMidRange.empty(); ++j) {
-
             coordinate = dMidRange.top().coordinate;
             range = dMidRange.top().range;
             dMidRange.pop();
-
             if (range > 1) {
                 if (range % 2 == 0) {//чет или нечет
                     midRange = range / 2;
                 } else {
                     midRange = ceil(double(range) / 2);
                 }
-
                 if (midRange - 1 > 0) {
                     dMidRange.push({midRange - 1, coordinate});
                 }
                 if (coordinate + range - midRange > 0) {
                     dMidRange.push({range - midRange, coordinate + midRange});
                 }
-
-                vec[coordinate+midRange-1] = j+1;
+                vec[coordinate + midRange - 1] = j + 1;
             } else {
-                vec[coordinate] = j+1;
+                vec[coordinate] = j + 1;
             }
 
         }
         vResult.push_back(vec);
     }
-    for (auto upEl:vResult) {
-        for (auto downEl:upEl) {
+    for (auto upEl: vResult) {
+        for (auto downEl: upEl) {
             cout << downEl << " ";
         }
         cout << endl;
